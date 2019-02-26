a7e4cd0444709d8b9a0f3aec23c6d9c85d48dbe7
Leonard Busenah <busenah@gmail.com>
2019-02-26 15:44:49 -0500

    commit4

--- controllers/user.js
+++ controllers/user.js
@@ -5,13 +5,16 @@ module.exports = {
   getSignup: function(req, res) {
     res.render("user/signup", { error: req.flash("error") });
   },
+  //   setting up signup for new users
   postSignup: function(req, res) {
     const { username, password } = req.body;
     User.register(new User({ username }), password)
       .then(user => {
+        //   allowing new users to get a new account and indicate
+        // to them the success of the new account creation
         const authenticate = passport.authenticate("local");
         authenticate(req, res, function() {
-          req.flash("success", "You created an account!");
+          req.flash("Congratulations", "You now have an account!");
           res.redirect("/");
         });
       })
@@ -20,6 +23,7 @@ module.exports = {
         res.redirect("/signup");
       });
   },
+  //   setting up login for returning users
   getLogin: function(req, res) {
     res.render("user/login", {
       error: req.flash("error"),
@@ -32,17 +36,19 @@ module.exports = {
       user,
       info
     ) {
+      //  if a new user logs in without having signed up, an error message is generated
       if (err || !user) {
         req.flash("error", info.message);
         res.redirect("/login");
       }
 
+      //  if a returning user logs in without the correct credentials, they are returned to login
       req.logIn(user, function(err) {
         if (err) {
           req.flash("error", err.message);
           return res.redirect("/login");
         }
-
+        //  allow a successful login
         req.flash("Your login has been successful", "You are logged in");
         return res.redirect("/");
       });
