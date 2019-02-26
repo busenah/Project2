fc631d0b7e07f2392ef18a24691ddc771865c491
Leonard Busenah <busenah@gmail.com>
2019-02-26 14:51:57 -0500

    adding on controllers

--- controllers/item.js
+++ controllers/item.js
@@ -1,5 +1,57 @@
-// my project is about a car tracker app that reminds its owner on the next maintenance date
+const { Item } = require("../models / index");
 
-// it has 2 models: the car and the maintenance garage
+module.exports = {
+  new: function(req, res) {
+    res.render("item/new");
+  },
+  create: function(req, res) {
+    const { name, description, priority, status } = req.body;
+    Item.create({
+      name,
+      description,
+      priority,
+      status
+    }).then(item => {
+      res.redirect(`/item/${item._id}`);
+    });
+  },
+  show: function(req, res) {
+    Item.findById(req.params.id).then(item => {
+      res.render("item/show", { item });
+    });
+  },
+  edit: function(req, res) {
+    Item.findById(req.params.id).then(item => {
+      res.render("item/edit", { item });
+    });
+  },
+  update: function(req, res) {
+    console.log(req.body);
+    const { name, description, status, priority } = req.body;
 
-//
+    Item.findOneAndUpdate(
+      req.params.id,
+      {
+        name,
+        description,
+        status,
+        priority
+      },
+      {
+        runValidators: true
+      }
+    )
+      .then(item => {
+        res.redirect(`/item/${item._id}`);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  delete: function(req, res) {
+    Item.remove({ _id: req.params.id }).then(item => {
+      console.log(item);
+      res.redirect("/");
+    });
+  }
+};
