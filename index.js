31d6532e56f2cccd5a57bd607a5e8ad252d8c2d1
Leonard Busenah <busenah@gmail.com>
2019-02-27 11:26:16 -0500

    added the body parser

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const parser = require("body-parser");
+
+app.use(
+  parser.urlencoded({
+    extended: true
+  })
+);
 
 app.set("view engine", "hbs");
 
