6fcacc4a5d0beaef9d99cb87c710054f214d17ae
Leonard Busenah <busenah@gmail.com>
2019-02-28 11:29:26 -0500

    added onto routes/user.js

---
+++ routes/user.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user");
+
+// allows sign up
+router.get("/signup", userController.getSignup);
+router.post("/signup", userController.postSignup);
+
+// allows for log in
+router.get("/login", userController.getLogin);
+router.post("/login", userController.postLogin);
+
+// allows for log out
+router.get("/logout", userController.getLogout);
+
+module.exports = router;
