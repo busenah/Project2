ae900b1eed4eae496333414cdd29f66aa2d8f531
Leonard Busenah <busenah@gmail.com>
2019-03-07 17:49:46 -0500

    adding on comment.js with Roger's help

--- models/comment.js
+++ models/comment.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const CommentSchema = new Schema({
   title: String,
   comment: String
 });
 
-module.exports = UserSchema;
+module.exports = CommentSchema;
